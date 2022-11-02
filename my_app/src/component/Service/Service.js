7155de47af95c72f6c000eeb58005a00e5cbf9ab
AdibRasel <97701792+AdibRasel@users.noreply.github.com>
2022-11-02 06:29:00 +0600

    code
    code

--- my_app/src/component/Service/Service.js
+++ my_app/src/component/Service/Service.js
@@ -30,6 +30,32 @@ import Row from 'react-bootstrap/Row';
 function Service() {
 
 
+    AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
    
 
     // counter code 
@@ -47,32 +73,7 @@ function Service() {
 
 
 
-    AOS.init();
 
-        // You can also pass an optional settings object
-        // below listed default settings
-        AOS.init({
-        // Global settings:
-        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-        initClassName: 'aos-init', // class applied after initialization
-        animatedClassName: 'aos-animate', // class applied on animation
-        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-        
-
-        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 20, // offset (in px) from the original trigger point
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 400, // values from 0 to 3000, with step 50ms
-        easing: 'ease', // default easing for AOS animations
-        once: false, // whether animation should happen only once - while scrolling down
-        mirror: false, // whether elements should animate out while scrolling past them
-        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-        });
 
 
 
@@ -80,15 +81,11 @@ function Service() {
   return (
     <div className='Service_Box'>
 
+        
+
         <div className="section1">
             
-            <div className="s1_left_side"   data-aos="flip-left"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center"  >
+            <div className="s1_left_side"   data-aos="flip-left" >
                 <h1>কেন আমাদের কাছ থেকে আন্তর্জাতিক রুটের টিকেট করবেন?</h1>
 
                 <ul>
@@ -104,13 +101,7 @@ function Service() {
 
             </div>
 
-            <div className="s1_right_side"  data-aos="flip-right"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center"   >
+            <div className="s1_right_side"  data-aos="flip-right">
                 <h1>Get Free Quotation</h1>
                 <form>      
                     <input name="name" type="text" class="feedback-input" placeholder="Name" />   
@@ -130,20 +121,8 @@ function Service() {
                 
                 <div className="s2_left_side_box">
 
-                    <p   data-aos="fade-down"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center" >Trip Tricks is the Best Travel and Tourism Agency in Dhaka Bangladesh.</p>
-                    < img src={govtAgency} alt=""   data-aos="fade-up-right"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center"  />
+                    <p   data-aos="fade-down">Trip Tricks is the Best Travel and Tourism Agency in Dhaka Bangladesh.</p>
+                    < img src={govtAgency} alt=""   data-aos="fade-up-right"/>
 
                 </div>
 
